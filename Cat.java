df32d53bc4e335b0c1a4bb0469b2049373f61df3
Ian Park <19ParkI@flintridgeprep.org>
2018-11-19 23:44:43 -0800

    finished cat class I think

--- Cat.java
+++ Cat.java
@@ -1,3 +1,24 @@
-
 public class Cat {
+    private String name;/*creates a name variable*/
+    private double weight;/*creates a weight value variable*/
+    void setName (String nName){
+        name = nName;
+    }
+    void setWeight (double wWeight){
+        weight = wWeight;
+    }
+    String getName (){
+        return name;
+    }
+    double getWeight (){
+        return weight;
+    }
+    public Cat(){
+    }
+    public Cat(String nName){
+        name = nName;
+    }
+    void meow(){
+        System.out.println("Meow");
+    }
 }
